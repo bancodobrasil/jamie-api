65a4e30aa2456c58ed15396de9537330b5a42a30
Elias Feijó <eliasfeijo.dev@gmail.com>
2022-12-21 08:42:52 -0300

    feat: MenuItem entity integrated with typeorm

--- src/menu-items/entities/menu-item.entity.ts
+++ src/menu-items/entities/menu-item.entity.ts
@@ -1,20 +1,41 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { GraphQLJSONObject } from 'src/common/graphql/types/json.type';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @ObjectType()
+@Entity('menu_items')
 export class MenuItem {
   @Field(() => Int)
+  @PrimaryGeneratedColumn()
   id: number;
 
   @Field()
+  @Column()
   label: string;
 
   @Field(() => Int)
+  @Column()
   order: number;
-
-  @Field(() => MenuItem, { nullable: true })
-  children?: MenuItem[];
 
   @Field(() => GraphQLJSONObject, { nullable: true })
   meta?: object;
+
+  @Field(() => MenuItem, { nullable: true })
+  @OneToMany(() => MenuItem, (menuItem) => menuItem.parent)
+  children?: MenuItem[];
+
+  @ManyToOne(() => MenuItem, (menuItem) => menuItem.children)
+  @JoinColumn()
+  parent?: MenuItem;
+
+  @Field(() => Int, { nullable: true })
+  @Column({ nullable: true })
+  parentId?: number;
 }
