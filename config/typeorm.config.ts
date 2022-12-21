e8e43d38a0d323c74da5b4dd4a355310a41e99cc
Elias Feijó <eliasfeijo.dev@gmail.com>
2022-12-21 07:49:16 -0300

    feat: typeorm config

---
+++ config/typeorm.config.ts
@@ -0,0 +1,22 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { join } from 'path';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+type TypeOrmConfig = (...args: any[]) => TypeOrmModuleOptions;
+
+export const typeOrmConfig: TypeOrmConfig = (configService: ConfigService) => {
+  return {
+    type: 'mysql',
+    host: configService.get('JAMIE_API_DATABASE_HOST'),
+    port: +configService.get('JAMIE_API_DATABASE_PORT'),
+    username: configService.get('JAMIE_API_DATABASE_USER'),
+    password: configService.get('JAMIE_API_DATABASE_PASSWORD'),
+    database: configService.get('JAMIE_API_DATABASE_NAME'),
+    entities: [join(__dirname, '..', 'src', '**', '*.entity{.js,.ts}')],
+    migrationsRun: true,
+    migrations: [join(__dirname, '..', 'migrations', '*{.js,.ts}')],
+    namingStrategy: new SnakeNamingStrategy(),
+    logging: true,
+  };
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,18 @@ import GraphQLJSON, {
   GraphQLJSONObject,
 } from './common/graphql/types/json.type';
 import { MenusModule } from './menus/menus.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from 'config/typeorm.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: typeOrmConfig,
+      inject: [ConfigService],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
