d43e9f30b2cc1b349ae471b4f9cdc3acbacd4ac1
Elias Feijó <eliasfeijo.dev@gmail.com>
2022-12-21 05:56:48 -0300

    feat: GraphQLJSON and GraphQLJSONObject scalar types

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,19 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import GraphQLJSON, {
+  GraphQLJSONObject,
+} from './common/graphql/types/json.type';
+import { MenusModule } from './menus/menus.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      resolvers: { JSON: GraphQLJSON, JSONObject: GraphQLJSONObject },
     }),
+    MenusModule,
   ],
   controllers: [AppController],
   providers: [AppService],
