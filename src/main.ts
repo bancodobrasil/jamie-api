43ad39659a5acdf1d0f6635dc8b092ddc16ee43b
Raphael Pinto <raphaelcpinto@gmail.com>
2023-01-17 11:20:57 -0300

    fix: port

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import tracer from './tracer';
 
+const { PORT = 5000 } = process.env;
+
 async function bootstrap() {
   await tracer.start();
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
