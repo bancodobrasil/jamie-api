5ba1e0765d2813b98fef34b36a923baa4ea9a57d
Raphael Pinto <raphaelcpinto@gmail.com>
2023-01-24 15:26:33 -0300

    fix: metrics

--- src/metrics.interceptor.ts
+++ src/metrics.interceptor.ts
@@ -16,7 +16,7 @@ export class MetricsInterceptor implements NestInterceptor {
   private readonly skipPaths = [/\/metrics.*/];
 
   constructor(
-    @InjectMetric('http_requests_traffic') private traffic : Counter<string>,
+    @InjectMetric('http_requests_traffic') private traffic: Counter<string>,
     @InjectMetric('http_requests_errors_count')
     private errors: Counter<string>,
     @InjectMetric('http_requests_latency') private latency: Histogram<string>,
@@ -26,7 +26,6 @@ export class MetricsInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler) {
     this.logger.debug({ interceptor: 'metrics', type: context.getType() });
     if (context.getType() === 'http') {
-      this.saturation.inc();
       return this.countHttpCall(context, next);
     }
     // TODO countGraphQLCall - qraphql
@@ -45,6 +44,8 @@ export class MetricsInterceptor implements NestInterceptor {
       return next.handle();
     }
 
+    this.saturation.inc({ endpoint, method });
+
     const end = this.latency.startTimer({ endpoint, method });
 
     return next.handle().pipe(
@@ -53,7 +54,7 @@ export class MetricsInterceptor implements NestInterceptor {
           this.errors.inc({ endpoint, method });
         }
         this.traffic.inc({ endpoint, method });
-        this.saturation.dec();
+        this.saturation.dec({ endpoint, method });
         end();
       }),
     );
