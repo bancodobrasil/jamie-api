e3e82e8b0d843a6ac63a6995945867d938274932
Raphael Pinto <raphaelcpinto@gmail.com>
2023-01-31 11:10:20 -0300

    feat: initied

--- src/logging.interceptor.ts
+++ src/logging.interceptor.ts
@@ -20,11 +20,48 @@ export class LoggingInterceptor implements NestInterceptor {
     if (context.getType() === 'http') {
       return this.logHttpCall(context, next);
     }
-    // TODO logGraphQLCall - qraphql
+    if (context.getType().toString() === 'qraphql') {
+      return this.logGraphQLCall(context, next);
+    }
     return next.handle();
   }
 
   private logHttpCall(context: ExecutionContext, next: CallHandler) {
+    const request = context.switchToHttp().getRequest();
+    const userAgent = request.get('user-agent') || '';
+    const { ip, method, path: url } = request;
+    const correlationKey = uuidv4();
+    const userId = request.user?.userId;
+
+    if (this.skipPaths.find((p) => p.test(url))) {
+      this.logger.verbose(`Skipped path "${url}"`);
+      return next.handle();
+    }
+
+    this.logger.debug(
+      `[${correlationKey}] ${method} ${url} ${userId} ${userAgent} ${ip}: ${
+        context.getClass().name
+      } ${context.getHandler().name}`,
+    );
+
+    const now = Date.now();
+    return next.handle().pipe(
+      tap(() => {
+        const response = context.switchToHttp().getResponse();
+
+        const { statusCode } = response;
+        const contentLength = response.get('content-length');
+
+        this.logger.debug(
+          `[${correlationKey}] ${method} ${url} ${statusCode} ${contentLength}: ${
+            Date.now() - now
+          }ms`,
+        );
+      }),
+    );
+  }
+
+  private logGraphQLCall(context: ExecutionContext, next: CallHandler) {
     const request = context.switchToHttp().getRequest();
     const userAgent = request.get('user-agent') || '';
     const { ip, method, path: url } = request;
