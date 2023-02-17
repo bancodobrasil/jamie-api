c9c055b941fb807832519925d42fe0ca000bfc26
C1320805 Elias Feijo de Almeida Pereira Lima <c1320805@interno.bb.com.br>
2023-02-16 22:10:17 -0300

    feat: MenuSubscriber beforeInsert and beforeUpdate validate meta unique fields

--- src/menus/entities/subscribers/menu.subscriber.ts
+++ src/menus/entities/subscribers/menu.subscriber.ts
@@ -1,8 +1,11 @@
+import FieldValidationError from 'src/common/errors/field-validation.error';
 import { MenuItem } from 'src/menu-items/entities/menu-item.entity';
+import { MenuMeta } from 'src/menus/objects/menu-meta.object';
 import {
   EntitySubscriberInterface,
   EventSubscriber,
   InsertEvent,
+  UpdateEvent,
 } from 'typeorm';
 import { Menu } from '../menu.entity';
 
@@ -11,10 +14,78 @@ export class MenuSubscriber implements EntitySubscriberInterface<Menu> {
   listenTo() {
     return Menu;
   }
+
+  beforeInsert(event: InsertEvent<Menu>): void {
+    const { meta } = event.entity;
+    if (meta?.length) {
+      this.validateMeta(meta);
+    }
+  }
+
+  beforeUpdate(event: UpdateEvent<Menu>): void {
+    const { meta } = event.entity;
+    if (meta?.length) {
+      this.validateMeta(meta);
+    }
+  }
+
   async afterInsert(event: InsertEvent<Menu>) {
     const menu = await this.setMenu(event.entity);
     await event.manager.save(menu, { reload: true });
   }
+
+  private validateMeta(meta: MenuMeta[]) {
+    const metaWithIndex = meta.map((m, index) => ({ ...m, index }));
+    const repeatedMetaIds = metaWithIndex.filter((m) => {
+      return metaWithIndex.find((m2) => m2.id === m.id && m2.index !== m.index);
+    });
+    const repeatedMetaNames = metaWithIndex.filter((m) => {
+      return metaWithIndex.find(
+        (m2) => m2.name === m.name && m2.index !== m.index,
+      );
+    });
+    const repeatedMetaOrders = metaWithIndex.filter((m) => {
+      return metaWithIndex.find(
+        (m2) => m2.order === m.order && m2.index !== m.index,
+      );
+    });
+    if (
+      !repeatedMetaIds.length &&
+      !repeatedMetaNames.length &&
+      !repeatedMetaOrders.length
+    ) {
+      return;
+    }
+    const errors = {};
+    metaWithIndex.forEach((m) => {
+      if (repeatedMetaIds.find((m2) => m2.id === m.id)) {
+        errors[`meta[${m.index}]`] = {
+          ...errors[`meta[${m.index}]`],
+          id: {
+            errors: `Menu meta ids must be unique. Found repeated id: ${m.id}`,
+          },
+        };
+      }
+      if (repeatedMetaNames.find((m2) => m2.id === m.id)) {
+        errors[`meta[${m.index}]`] = {
+          ...errors[`meta[${m.index}]`],
+          name: {
+            errors: `Menu meta names must be unique. Found repeated name: ${m.name}`,
+          },
+        };
+      }
+      if (repeatedMetaOrders.find((m2) => m2.id === m.id)) {
+        errors[`meta[${m.index}]`] = {
+          ...errors[`meta[${m.index}]`],
+          order: {
+            errors: `Menu meta orders must be unique. Found repeated order: ${m.order}`,
+          },
+        };
+      }
+    });
+    throw new FieldValidationError(errors);
+  }
+
   private async setMenu(menu: Menu): Promise<Menu> {
     const items = await menu.items;
     if (items?.length) {
