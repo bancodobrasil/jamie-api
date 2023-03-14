c467d3ea570a65125246ed4621764949dd59a88d
C1320805 Elias Feijo de Almeida Pereira Lima <c1320805@interno.bb.com.br>
2023-03-14 07:23:29 -0300

    feat: removing unused helpers

--- src/common/helpers/template.helper.ts
+++ src/common/helpers/template.helper.ts
@@ -1,6 +1,5 @@
 /* eslint-disable prefer-rest-params */
 import Handlebars from 'handlebars';
-import { MenuItem } from 'src/menu-items/entities/menu-item.entity';
 
 export default class TemplateHelpers {
   public static setup() {
@@ -16,11 +15,6 @@ export default class TemplateHelpers {
     Handlebars.registerHelper('json', TemplateHelpers.json);
     Handlebars.registerHelper('jsonFormatter', TemplateHelpers.jsonFormatter);
     Handlebars.registerHelper('withIndent', TemplateHelpers.withIndent);
-    Handlebars.registerHelper(
-      'renderItemsJSON',
-      TemplateHelpers.renderItemsJSON,
-    );
-    Handlebars.registerHelper('renderItemsXML', TemplateHelpers.renderItemsXML);
   }
 
   public static registerPartials() {
@@ -85,35 +79,6 @@ export default class TemplateHelpers {
       indent || options.hash.spaces ? ' '.repeat(options.hash.spaces) : '\t';
     const lines = options.fn(this).split('\n');
     return lines.map((line) => indent + line).join('\n');
-  }
-
-  public static renderItemsJSON(
-    items: MenuItem[],
-    options: Handlebars.HelperOptions,
-  ) {
-    if (!items || !items.length) return TemplateHelpers.json([], options);
-    const renderItem = (item: MenuItem): Record<string, unknown> => {
-      if (!item.enabled) return null;
-      return JSON.parse(item.template);
-    };
-    return JSON.stringify(
-      items.map(renderItem).filter((v) => v !== null),
-      null,
-      options.hash.spaces,
-    );
-  }
-
-  public static renderItemsXML(items: MenuItem[]) {
-    if (!items || !items.length) return '';
-    const renderItem = (item: MenuItem): string => {
-      if (!item.enabled) return '';
-      return item.template;
-    };
-    let xml = '';
-    items.forEach((item) => {
-      xml += `${renderItem(item)}\n`;
-    });
-    return xml.substring(0, xml.length - 1);
   }
 
   public static partials = {
