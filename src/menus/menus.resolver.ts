0c15f6574b2427d0112c5c4b387247657e34c798
C1320805 Elias Feijo de Almeida Pereira Lima <c1320805@interno.bb.com.br>
2023-03-16 03:17:52 -0300

    feat: role-based authorization resolvers

--- src/menus/menus.resolver.ts
+++ src/menus/menus.resolver.ts
@@ -8,37 +8,44 @@ import { FindMenuSortArgs } from './args/find-menu-sort.arg';
 import { CreateMenuRevisionInput } from './inputs/create-menu-revision.input';
 import { RenderMenuTemplateInput } from './inputs/render-menu-template.input';
 import { RenderMenuItemTemplateInput } from './inputs/render-menu-item-template.input';
+import { Roles } from 'nest-keycloak-connect';
 
 @Resolver(() => Menu)
 export class MenusResolver {
   constructor(private readonly menusService: MenusService) {}
 
   @Mutation(() => Menu)
+  @Roles({ roles: ['realm:editor'] })
   createMenu(@Args('createMenuInput') createMenuInput: CreateMenuInput) {
     return this.menusService.create(createMenuInput);
   }
 
   @Query(() => MenuConnection, { name: 'menus' })
+  @Roles({ roles: ['realm:reader'] })
   findAll(@Args() pagination: PaginationArgs, @Args() sort: FindMenuSortArgs) {
     return this.menusService.findAll(pagination, sort);
   }
 
   @Query(() => Menu, { name: 'menu' })
+  @Roles({ roles: ['realm:reader'] })
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.menusService.findOne(id);
   }
 
   @Mutation(() => Menu)
+  @Roles({ roles: ['realm:editor'] })
   updateMenu(@Args('updateMenuInput') updateMenuInput: UpdateMenuInput) {
     return this.menusService.update(updateMenuInput.id, updateMenuInput);
   }
 
   @Mutation(() => Boolean)
+  @Roles({ roles: ['realm:editor'] })
   removeMenu(@Args('id', { type: () => Int }) id: number) {
     return this.menusService.remove(id);
   }
 
   @Mutation(() => Menu)
+  @Roles({ roles: ['realm:editor'] })
   createRevision(
     @Args('createMenuRevisionInput')
     createMenuRevisionInput: CreateMenuRevisionInput,
@@ -47,6 +54,7 @@ export class MenusResolver {
   }
 
   @Mutation(() => Menu)
+  @Roles({ roles: ['realm:editor'] })
   restoreRevision(
     @Args('menuId', { type: () => Int }) menuId: number,
     @Args('revisionId', { type: () => Int }) revisionId: number,
@@ -55,6 +63,7 @@ export class MenusResolver {
   }
 
   @Mutation(() => Menu)
+  @Roles({ roles: ['realm:manager'] })
   publishRevision(
     @Args('menuId', { type: () => Int }) menuId: number,
     @Args('revisionId', { type: () => Int }) revisionId: number,
@@ -63,6 +72,7 @@ export class MenusResolver {
   }
 
   @Query(() => String)
+  @Roles({ roles: ['realm:reader'] })
   renderMenuTemplate(
     @Args('renderMenuTemplateInput')
     renderMenuTemplateInput: RenderMenuTemplateInput,
@@ -71,6 +81,7 @@ export class MenusResolver {
   }
 
   @Query(() => String)
+  @Roles({ roles: ['realm:reader'] })
   renderMenuItemTemplate(
     @Args('renderMenuItemTemplateInput')
     renderMenuItemTemplateInput: RenderMenuItemTemplateInput,
