dd8a60edb44021b17d3255a911e302a0badcd435
C1320805 Elias Feijo de Almeida Pereira Lima <c1320805@interno.bb.com.br>
2023-03-21 20:06:58 -0300

    feat: KeycloakAccessToken interface

--- src/common/types/express.d.ts
+++ src/common/types/express.d.ts
@@ -1,12 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Request } from 'express';
+import { KeycloakAccessToken } from './keycloak.type';
 
 declare global {
   namespace Express {
     export interface Request {
-      user?: {
-        sub: string;
-      };
+      user?: KeycloakAccessToken;
     }
   }
 }
---
+++ src/common/types/keycloak.type.ts
@@ -0,0 +1,29 @@
+export interface KeycloakAccessToken {
+  jti: string;
+  exp: number;
+  nbf: number;
+  iat: number;
+  iss: string;
+  sub: string;
+  typ: string;
+  azp: string;
+  acr: string;
+  aud: string | string[];
+  realm_access: {
+    roles: string[];
+  };
+  resource_access: {
+    [key: string]: {
+      roles: string[];
+    };
+  };
+  scope: string;
+  email_verified?: boolean;
+  name?: string;
+  preferred_username?: string;
+  given_name?: string;
+  family_name?: string;
+  email?: string;
+  groups?: string[];
+  username?: string;
+}
