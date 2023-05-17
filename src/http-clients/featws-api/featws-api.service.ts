34ee915c07c67202108e8a9c39335ac575c1debe
C1320805 Elias Feijo de Almeida Pereira Lima <c1320805@interno.bb.com.br>
2023-05-17 13:37:44 -0300

    feat: FeatwsApiService updateRulesheet

--- src/http-clients/featws-api/featws-api.service.ts
+++ src/http-clients/featws-api/featws-api.service.ts
@@ -4,6 +4,7 @@ import { isAxiosError } from 'axios';
 import { firstValueFrom } from 'rxjs';
 import { CreateRulesheetDto } from './dtos/create-rulesheet.dto';
 import { RulesheetResponseDto } from './dtos/rulesheet-response.dto';
+import { UpdateRulesheetDto } from './dtos/update-rulesheet.dto';
 import { FeatwsApiError } from './featws-api.errors';
 import { MODULE_OPTIONS_TOKEN } from './featws-api.module-definition';
 import { FeatwsApiOptions } from './featws-api.options';
@@ -32,6 +33,32 @@ export class FeatwsApiService {
         this.httpService.post<RulesheetResponseDto>(
           `${this.options.url}/v1/rulesheets`,
           createRulesheetDto,
+          {
+            headers: this.defaultHeaders,
+          },
+        ),
+      );
+      return response.data;
+    } catch (error) {
+      if (isAxiosError(error) && error.response) {
+        throw new FeatwsApiError(
+          error.response.data?.message || error.message,
+          {
+            status: error.response.status,
+          },
+        );
+      } else {
+        throw new FeatwsApiError(error.message);
+      }
+    }
+  }
+
+  async updateRulesheet(updateRulesheetDto: UpdateRulesheetDto) {
+    try {
+      const response = await firstValueFrom(
+        this.httpService.put<RulesheetResponseDto>(
+          `${this.options.url}/v1/rulesheets`,
+          updateRulesheetDto,
           {
             headers: this.defaultHeaders,
           },
