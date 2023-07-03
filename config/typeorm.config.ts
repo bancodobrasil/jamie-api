a2989d10c046bc48838b9dae202cdfef979e564e
Raphael Pinto <raphaelcpinto@gmail.com>
2023-07-03 19:46:38 -0300

    fix: oracle service name

--- config/typeorm.config.ts
+++ config/typeorm.config.ts
@@ -13,7 +13,7 @@ export const typeOrmConfig: TypeOrmConfig = (configService: ConfigService) => {
     username: configService.get('JAMIE_API_DATABASE_USER'),
     password: configService.get<string>('JAMIE_API_DATABASE_PASSWORD', 'changeme'),
     database: configService.get<string>('JAMIE_API_DATABASE_NAME', 'jamie-api'),
-    sid: configService.get<string|undefined>('JAMIE_API_DATABASE_SERVICE_NAME'),
+    serviceName: configService.get<string|undefined>('JAMIE_API_DATABASE_SERVICE_NAME'),
     timezone: 'Z',
     entities: [join(__dirname, '..', 'src', '**', '*.entity{.js,.ts}')],
     subscribers: [join(__dirname, '..', 'src', '**', '*.subscriber{.js,.ts}')],
